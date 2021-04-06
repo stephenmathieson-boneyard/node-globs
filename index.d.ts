5bf4f58b6457af6d395c6a88df09d185aa4efcbf
Oliver Foster <oliver.foster@kineo.com>
2021-04-06 14:57:06 +0100

    Added async/await support

--- index.d.ts
+++ index.d.ts
@@ -3,17 +3,17 @@ import glob = require("glob");
 /**
  * Performs an asynchronous glob search.
  * @param pattern Pattern or patterns to be matched.
- * @param cb The callback invoked when the search completes.
+ * @param [cb] The callback invoked when the search completes.
  */
-declare function G(pattern: string|string[], cb: (err: Error | null, matches: string[]) => void): void;
+declare function G(pattern: string|string[], cb?: (err: Error | null, matches: string[]) => void): Promise<T>;
 
 /**
  * Performs an asynchronous glob search.
  * @param pattern Pattern or patterns to be matched.
  * @param options The glob options to use.
- * @param cb The callback invoked when the search completes.
+ * @param [cb] The callback invoked when the search completes.
  */
-declare function G(pattern: string|string[], options: glob.IOptions, cb: (err: Error | null, matches: string[]) => void): void;
+declare function G(pattern: string|string[], options: glob.IOptions, cb?: (err: Error | null, matches: string[]) => void): Promise<T>;
 
 declare namespace G {
 
